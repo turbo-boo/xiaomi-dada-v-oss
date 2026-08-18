0c4850558df0ccf2fb90b1671c01c384d3d3fc4b
boo <59400127+turbo-boo@users.noreply.github.com>
2026-08-18 22:19:05 +0900

    mca: restore SOC limit public ABI

---
+++ drivers/power/supply/mca/include/mca/strategy/strategy_soc_limit.h
@@ -0,0 +1,8 @@
+#ifndef _MCA_STRATEGY_STRATEGY_SOC_LIMIT_H_
+#define _MCA_STRATEGY_STRATEGY_SOC_LIMIT_H_
+
+#include <linux/types.h>
+
+void soc_limit_process(bool enable, int soc_limit_thre);
+
+#endif
